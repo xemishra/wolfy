184bddc50fbd1130a807d0df924b07b6bcdf795b
Shivanand Mishra <xemishra@gmail.com>
2026-06-09 17:19:39 +0530

    feat: implement database connectivity module & paths

---
+++ app/core/__init__.py
@@ -0,0 +1,12 @@
+from app.core.database import db, ensure_indexes
+from app.core.paths import ASSETS_DIR, ROOT_DIR, STATIC_DIR, TEMPLATES_DIR, UPLOAD_DIR
+
+__all__ = [
+    "db",
+    "ensure_indexes",
+    "ROOT_DIR",
+    "STATIC_DIR",
+    "TEMPLATES_DIR",
+    "ASSETS_DIR",
+    "UPLOAD_DIR",
+]
